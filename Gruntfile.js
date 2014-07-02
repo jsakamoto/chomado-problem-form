1ebe34f579cbd9538b8b9bc1e35ac9fef83f4dd8
jsakamoto <junichi.jp@gmail.com>
2014-07-03 00:04:00 +0900

    Improve building environment.

---
+++ Gruntfile.js
@@ -0,0 +1,50 @@
+module.exports = function (grunt) {
+    grunt.initConfig({
+        // configuration for ts
+        ts: {
+            options: {
+                target: 'es5',
+                module: 'commonjs',
+                noImplicitAny: true,
+                removeComments: false
+            },
+            main: {
+                src: ['*.ts'],
+                outDir: '.'
+            }
+        },
+
+        // configuration for bower
+        bower: {
+            install: {
+                options: {
+                    install: true
+                }
+            }
+        },
+
+        // configuration for tsd
+        tsd: {
+            install: {
+                options: {
+                    command: 'reinstall',
+                    latest: false,
+                    config: './tsd.json'
+                }
+            }
+        },
+
+        watch: {
+            files: ['*.ts'],
+            tasks: ['ts:main']
+        }
+    });
+
+    grunt.registerTask('default', ['ts:main']);
+    grunt.registerTask('setup', ['bower', 'tsd']);
+
+    grunt.loadNpmTasks('grunt-ts');
+    grunt.loadNpmTasks('grunt-tsd');
+    grunt.loadNpmTasks('grunt-bower-task');
+    grunt.loadNpmTasks('grunt-contrib-watch');
+};
---
+++ grunt.cmd
@@ -0,0 +1,1 @@
+ @call .\node_modules\.bin\grunt.cmd %*
