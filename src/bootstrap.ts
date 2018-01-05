b0c811d889754c671b570fd28a4a06b6cd79b953
jsakamoto <junichi.jp@gmail.com>
2018-01-05 16:00:26 +0900

    Enable producton mode.

--- src/bootstrap.ts
+++ src/bootstrap.ts
@@ -1,9 +1,14 @@
 import 'reflect-metadata';
 import 'zone.js';
+import { enableProdMode } from '@angular/core';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic'
 import { AppModule } from './app.module'
 
 if (module.hot) {
     module.hot.accept();
 }
+else {
+    enableProdMode();
+}
+
 const modulePromise = platformBrowserDynamic().bootstrapModule(AppModule);
