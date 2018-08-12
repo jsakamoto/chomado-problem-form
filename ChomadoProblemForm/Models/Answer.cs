69df40912cec74a7b12377720abd8a0326117c8d
jsakamoto <junichi.jp@gmail.com>
2018-08-12 14:16:32 +0900

    Port to "Blazor"!

---
+++ ChomadoProblemForm/Models/Answer.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChomadoProblemForm.Models
+{
+    public class Answer
+    {
+        public int Q { get; set; }
+
+        public int? Value { get; set; }
+    }
+}
---
+++ ChomadoProblemForm/Models/Contributor.cs
@@ -0,0 +1,11 @@
+﻿namespace ChomadoProblemForm.Models
+{
+    public class Contributor
+    {
+        public string html_url { get; set; }
+
+        public string avatar_url { get; set; }
+
+        public string login { get; set; }
+    }
+}
