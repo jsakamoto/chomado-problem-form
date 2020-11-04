266412a5892a092b158299781aca8c140b667530
jsakamoto <junichi.jp@gmail.com>
2020-11-04 21:00:20 +0900

    Upgrade Blazor v.5.0 rc2

--- ChomadoProblemForm/Program.cs
+++ ChomadoProblemForm/Program.cs
@@ -1,7 +1,10 @@
-﻿using System.Threading.Tasks;
+﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using Microsoft.Extensions.DependencyInjection;
 using Skclusive.Material.Component;
+using Skclusive.Material.Core;
 
 namespace ChomadoProblemForm
 {
@@ -10,9 +13,9 @@ namespace ChomadoProblemForm
         public static async Task Main(string[] args)
         {
             var builder = WebAssemblyHostBuilder.CreateDefault(args);
-            builder.RootComponents.Add<App>("app");
+            builder.RootComponents.Add<App>("#app");
 
-            builder.Services.AddBaseAddressHttpClient();
+            builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
             builder.Services.TryAddMaterialServices(new MaterialConfigBuilder().Build());
 
             await builder.Build().RunAsync();
